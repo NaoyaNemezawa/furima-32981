ec3cb24308d0abe36c8e962799ee577a85050eaa
unya33ki <iimuzoku2@yahoo.co.jp>
2020-12-19 19:57:57 +0900

    ログアウト状態のユーザーは出品ページにいかずログイン画面へ移動する

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,5 @@
 class ItemsController < ApplicationController
+  before_action :move_to_index, except: [:index]
   def index
   end
 
@@ -7,7 +8,6 @@ class ItemsController < ApplicationController
   end
 
   def create
-    binding.pry
     items = Item.new(item_params)
     items.save
     redirect_to root_path
@@ -16,5 +16,11 @@ class ItemsController < ApplicationController
   private
   def item_params
     params.require(:item).permit(:image,:name,:description,:category_id,:state_id,:postage_charge_id,:prefecture_id,:shipment_date_id,:price).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to user_session_path
+    end
   end
 end
