de16092ce1e0047eb06a0e5f900b518a9d15a54b
unya33ki <iimuzoku2@yahoo.co.jp>
2020-12-19 19:47:16 +0900

    商品をデータベースへ保存する機能の作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,20 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @items = Item.new
+  end
+
+  def create
+    binding.pry
+    items = Item.new(item_params)
+    items.save
+    redirect_to root_path
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:image,:name,:description,:category_id,:state_id,:postage_charge_id,:prefecture_id,:shipment_date_id,:price).merge(user_id: current_user.id)
+  end
 end
