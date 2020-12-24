25e79c254af0e448fb219df5187c809a35127134
unya33ki <iimuzoku2@yahoo.co.jp>
2020-12-24 23:35:22 +0900

    Rubocopによる修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,9 +4,7 @@ class OrdersController < ApplicationController
   def index
     @order_address = OrderAddress.new
     @item = Item.find(params[:item_id])
-    if current_user.id == @item.user_id || Order.find_by(item_id:params[:item_id])
-      redirect_to root_path
-    end
+    redirect_to root_path if current_user.id == @item.user_id || Order.find_by(item_id: params[:item_id])
   end
 
   def create
@@ -17,21 +15,24 @@ class OrdersController < ApplicationController
       @order_address.save
       redirect_to root_path
     else
-      render "index"
+      render 'index'
     end
   end
 
   private
+
   def order_params
-    params.require(:order_address).permit(:postal_code,:prefecture_id,:city,:number,:building,:tel).merge(user_id: current_user.id,item_id: params[:item_id],token: params[:token]) 
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :number, :building, :tel).merge(
+      user_id: current_user.id, item_id: params[:item_id], token: params[:token]
+    )
   end
 
   def pay_item
-    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
     Payjp::Charge.create(
       amount: @item.price,
       card: order_params[:token],
-      currency: "jpy"
+      currency: 'jpy'
     )
   end
 end
